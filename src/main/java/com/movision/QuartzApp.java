fc14037cd818524d4e703d1326261d0dce429df6
ywgitlabadmin <zhuangyuhao711@163.com>
2017-03-13 20:22:13 +0800

    哈哈哈

--- src/main/java/com/movision/QuartzApp.java
+++ src/main/java/com/movision/QuartzApp.java
@@ -10,7 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  *
  * 该定时任务启动tomcat后，通过控制QuartzApp的启动来控制定时任务的开/关
  *
- * 啦啦啦啦啦了
+ * 啦啦啦啦啦了，哈哈哈哈哈
  *
  * @author zhuangyuhao
  * @since 15/7/13.
