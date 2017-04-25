2b9f7f9bd6f0cd2bc24f45fab864c2a699825d51
shuxf <shuxianfon1987@126.com>
2017-04-25 14:51:35 +0800

    修改图片扫描接口

--- src/main/java/com/movision/task/CleanScanAllImgTask.java
+++ src/main/java/com/movision/task/CleanScanAllImgTask.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.movision.utils.propertiesLoader.PropertiesLoader;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -39,9 +40,41 @@ public class CleanScanAllImgTask {
         log.info("活动内容中原图的存放路径>>>>>>>>>>>>>>>>>>>" + activeprotoimgpath);
         log.info("帖子内容中视频存放路径>>>>>>>>>>>>>>>>>>>" + postprotovideopath);
 
+        //定义标志位（0 未使用到 1 有使用到）
+        int flag = 0;//初始化为未使用到
+
+        //根据文件路径扫描所有带扫描的文件
+        String [] compressimgFileName = getFileName(compressimgpath);
+        String [] postprotoimgFileName = getFileName(postprotoimgpath);
+        String [] activeprotoimgFileName = getFileName(activeprotoimgpath);
+        String [] postprotovideoFileName = getFileName(postprotovideopath);
+        for(String name:compressimgFileName)
+        {
+            System.out.println(name);
+        }
+        for(String name:postprotoimgFileName)
+        {
+            System.out.println(name);
+        }
+        for(String name:activeprotoimgFileName)
+        {
+            System.out.println(name);
+        }
+        for(String name:postprotovideoFileName)
+        {
+            System.out.println(name);
+        }
+
+
         //扫描所有帖子的内容和封面进行检查
         for (int i = 0; i < allPostList.size(); i++) {
             log.info("当前处理的帖子id>>>>>>>>>>>>>>>>>>" + allPostList.get(i).getId());
         }
+    }
+
+    public static String [] getFileName(String path) {
+        File file = new File(path);
+        String [] fileName = file.list();
+        return fileName;
     }
 }
