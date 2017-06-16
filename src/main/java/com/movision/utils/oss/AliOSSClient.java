865c6e89c2da6d8092470085bb311aff84141543
shuxf <shuxianfon1987@126.com>
2017-06-16 16:58:00 +0800

    提交定时任务自动上传数据库备份文件到阿里云OSS静态资源服务器模块

--- src/main/java/com/movision/utils/oss/AliOSSClient.java
+++ src/main/java/com/movision/utils/oss/AliOSSClient.java
@@ -45,8 +45,8 @@ public class AliOSSClient {
     // accessKeyId和accessKeySecret是OSS的访问密钥，您可以在控制台上创建和查看，
     // 创建和查看访问密钥的链接地址是：https://ak-console.aliyun.com/#/。
     // 注意：accessKeyId和accessKeySecret前后都没有空格，从控制台复制时请检查并去除多余的空格。
-    private static String accessKeyId = "LTAINBMX7g4IDgiu";
-    private static String accessKeySecret = "kX1lPkhkTA3EM4QL995ottVndIa0GM";
+    private static String accessKeyId = "LTAITI0FOKgMZQty";
+    private static String accessKeySecret = "5x12H1yASg31OlPanhTCg0z0PzNr3t";
 
     // Bucket用来管理所存储Object的存储空间，详细描述请参看“开发人员指南 > 基本概念 > OSS基本概念介绍”。
     // Bucket命名规范如下：只能包括小写字母，数字和短横线（-），必须以小写字母或者数字开头，长度必须在3-63字节之间。
