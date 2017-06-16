86b286d828b69335ba1b48dad93e34e9d672a3a8
shuxf <shuxianfon1987@126.com>
2017-06-16 16:19:32 +0800

    提交定时任务自动上传数据库备份文件到阿里云OSS静态资源服务器模块

--- src/main/java/com/movision/utils/oss/AliOSSClient.java
+++ src/main/java/com/movision/utils/oss/AliOSSClient.java
@@ -39,17 +39,17 @@ public class AliOSSClient {
     // 链接地址是：https://help.aliyun.com/document_detail/oss/user_guide/oss_concept/endpoint.html?spm=5176.docoss/user_guide/endpoint_region
     // endpoint的格式形如“http://oss-cn-hangzhou.aliyuncs.com/”，注意http://后不带bucket名称，
     // 比如“http://bucket-name.oss-cn-hangzhou.aliyuncs.com”，是错误的endpoint，请去掉其中的“bucket-name”。
-    private static String endpoint = "http://oss-cn-hangzhou-internal.aliyuncs.com";// "http://oss-cn-hangzhou.aliyuncs.com";
+    private static String endpoint = PropertiesLoader.getValue("ali.domain");// "http://oss-cn-hangzhou.aliyuncs.com";
 
     // accessKeyId和accessKeySecret是OSS的访问密钥，您可以在控制台上创建和查看，
     // 创建和查看访问密钥的链接地址是：https://ak-console.aliyun.com/#/。
     // 注意：accessKeyId和accessKeySecret前后都没有空格，从控制台复制时请检查并去除多余的空格。
-    private static String accessKeyId = "MpIeQ3cwKShE2zcV";
-    private static String accessKeySecret = "0YdrhrEgFwMFc7A9DPDYbP0QxN7ejX";
+    private static String accessKeyId = PropertiesLoader.getValue("access.key.id");
+    private static String accessKeySecret = PropertiesLoader.getValue("access.key.secret");
 
     // Bucket用来管理所存储Object的存储空间，详细描述请参看“开发人员指南 > 基本概念 > OSS基本概念介绍”。
     // Bucket命名规范如下：只能包括小写字母，数字和短横线（-），必须以小写字母或者数字开头，长度必须在3-63字节之间。
-    private static String bucketName = "image-zhuhui8";
+    private static String bucketName = PropertiesLoader.getValue("file.bucket");
 
     // Object是OSS存储数据的基本单元，称为OSS的对象，也被称为OSS的文件。详细描述请参看“开发人员指南 > 基本概念 >
     // OSS基本概念介绍”。
